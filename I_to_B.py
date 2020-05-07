1fc62c45104f23b9f4c2de73334115ff72497199
gabytec <magaby.tec@gmail.com>
2020-05-07 10:46:54 -0600

    version 2
    se documento el código

--- I_to_B.py
+++ I_to_B.py
@@ -4,42 +4,39 @@ import tkinter as tk
 from tkinter import *
 import numpy as np
 
-
+#Función para crear el archivo binario de la imagen
 def pixel(img):
-    file = open("imagen.bin", "w")
-    for i in img:
-        for j in i:
-            valor = binario(j)
-            file.write(valor)
-            j = j + 1
-        j = 0
-        i = i + 1
-    file.close()
+    file = open("imagen.bin", "w") #se crear el archivo
+    for i in img: #ciclo para el alto
+        for j in i: #ciclo para el ancho
+            valor = binario(j) #se obtiene el pixel y se cambia a binario
+            file.write(valor) #se escribe el binario obtenido en el archivo
+    file.close() #se cierra el archivo
 
+#Función para cambiar un número a binario
 def binario(numero):
-    Num_binario = "{0:b}".format(numero)
-    if (len(Num_binario) < 8):
-        complemento = 8 - len(Num_binario)
-        i = 0
-        while i < complemento:
-            Num_binario = '0' + Num_binario
-            i = i + 1
-        return Num_binario
-    else:
-        return Num_binario
+    Num_binario = '{0:b}'.format(numero) #se cambia el número decimal a binario
+    if (len(Num_binario) < 8): #si el tamaño del binario es menor que 8
+        complemento = 8 - len(Num_binario) #se obtiene la diferencia 
+        i = 0 #se inicia el contador 0
+        while i < complemento: #ciclo
+            Num_binario = '0' + Num_binario #se le suena un 0 a la izquierda del número binario
+            i = i + 1#se suma un 1 al contador
+        return Num_binario #se devuelve en binario de 8 bits
+    else: #si el binario si tiene 8 bits
+        return Num_binario  #se devuelve en binario de 8 bits
     
-#Main    
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename(initialdir="/", title="Seleccione una imagen", filetypes=(("Image file", ("*.png","*.jpg","*.gif","*.bmp","*.jpeg"))                                                                                                ,("all files", "*.*")))
-#img=cv2.imread(file_path,0)
-img=cv2.imread(file_path)
-print(img)
-pixel(img)
-plt.imshow(img, cmap='gray')
-plt.xticks([]),plt.yticks([])
-plt.show()
+########################################### MAIN ################################
+root = tk.Tk() #pantalla para seleccionar la imagen
+root.withdraw() #se muestra la pantalla
+#se arma el path de la imagen
+file_path = filedialog.askopenfilename(initialdir="/", title="Seleccione una imagen", filetypes=(("Image file", ("*.png","*.jpg","*.gif","*.bmp","*.jpeg"))
+                                                                                                 ,("all files", "*.*")))
+#se abre la imagen
+img=cv2.imread(file_path,0)
 
-#cv2.imwrite("test.jpg", img)
+#se llama a la función donde se guarda el archivo 
+pixel(img)
+
 
 
